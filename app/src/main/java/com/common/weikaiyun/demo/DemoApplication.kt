f2813343b90ab5dc2a8aa0c6b50fe835498fe2a6
weikaiyun <wky19900505@gmail.com>
2020-10-11 15:38:00 +0800

    优化demo

--- app/src/main/java/com/common/weikaiyun/demo/DemoApplication.kt
+++ app/src/main/java/com/common/weikaiyun/demo/DemoApplication.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import android.content.Context
 import com.common.weikaiyun.R
 import com.common.changeskin.SkinManager
-import com.common.weikaiyun.demo.db.UserDatabase
 import com.weikaiyun.fragmentation.Fragmentation
 
 class DemoApplication: Application() {
@@ -12,7 +11,6 @@ class DemoApplication: Application() {
         super.onCreate()
         instance = this
         context = this
-        UserDatabase.getInstance()
 
         Fragmentation.builder() // 设置 栈视图 模式为 （默认）悬浮球模式   SHAKE: 摇一摇唤出  NONE：隐藏， 仅在Debug环境生效
             .stackViewMode(Fragmentation.BUBBLE)
